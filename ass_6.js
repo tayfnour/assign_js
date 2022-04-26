595cab825fed6d4c260ecc614c88ab83aea86d98
M.Hassane <abo.omar.tayf@gmail.com>
2022-04-27 01:00:53 +0300

    Create ass_6.js

---
+++ ass_6.js
@@ -0,0 +1,8 @@
+//declare a variable for every data type used in JavaScript 
+let myString = "This is a string";   //This is a string  variable
+let myNumber = 10;                   //This is a number variable
+let myBoolean = true;                //This is a boolean variable
+let myArray = [1, 2, 3, 4, 5];       //This is an array variable
+let myObject = {name: "John", age: 30}; //This is an object variable
+let myNull = null;                   //This is a null variable
+let myUndefined = undefined;         //This is an undefined variable
